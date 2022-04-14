e8b3b936af23dcbd3cd0a4495dadc52b800ebbfe
RomanReyes94 <Romanreyes94@knights.ucf.edu>
2022-04-14 11:17:18 -0400

    first commit

---
+++ src/BluetoothScanner.js
@@ -0,0 +1,250 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Button, PermissionsAndroid, ImageBackground, Platform, TouchableOpacity, Linking, StyleSheet,  container, SafeAreaView, Image, AppRegistry, ScrollView, TextInput,  } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
+
+export const manager = new BleManager();
+
+const requestPermission = async () => {
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+      title: "Request for Location Permission",
+      message: "Bluetooth Scanner requires access to Fine Location Permission",
+      buttonNeutral: "Ask Me Later",
+      buttonNegative: "Cancel",
+      buttonPositive: "OK"
+    }
+  );
+  return (granted === PermissionsAndroid.RESULTS.GRANTED);
+}
+
+// BlueetoothScanner does:
+// - access/enable bluetooth module
+// - scan bluetooth devices in the area
+// - list the scanned devices
+const BluetoothScanner = () => {
+  const [logData, setLogData] = useState([]);
+  const [logCount, setLogCount] = useState(0);
+  const [scannedDevices, setScannedDevices] = useState({});
+  const [deviceCount, setDeviceCount] = useState(0);
+
+  useEffect(() => {
+    manager.onStateChange((state) => {
+      const subscription = manager.onStateChange(async (state) => {
+        console.log(state);
+        const newLogData = logData;
+        newLogData.push(state);
+        await setLogCount(newLogData.length);
+        await setLogData(newLogData);
+        subscription.remove();
+      }, true);
+      return () => subscription.remove();
+    });
+  }, [manager]);
+
+  return (
+    <ScrollView style={{flex:1, padding:10}}>
+<TouchableOpacity onPress={() => { Linking.openURL(`tel:4071111111`) }}
+            onLongPress={() => { Linking.openURL(`tel:911`) }}
+            >
+            <Image 
+            style={styles.emergencyLogo} 
+            source={require('../assets/emergency.jpg')} 
+            />
+            </TouchableOpacity>
+      <ScrollView style={{flex:1, padding:10}}>
+        <Text style={{fontWeight: "bold"}}>Bluetooth Log ({logCount})</Text>
+        <FlatList
+          data={logData}
+          renderItem={({item}) => {
+            return (<Text>{item}</Text>)
+          }}
+        />
+            
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
+
+            <Text style={styles.Group45text}>ARGUS</Text>  
+
+        <Text> Name:</Text>  
+        <TextInput 
+        style={styles.input}
+        placeholder='e.g. John Doe'></TextInput>
+
+<Text> Age:</Text>  
+        <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 40'></TextInput>
+
+<Text> Blood Type:</Text>  
+        <TextInput 
+        style={styles.input}
+        placeholder='e.g. A+'></TextInput>
+
+<Text> Medications:</Text>  
+        <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. Tylenol'></TextInput>
+
+<Text> Allergies:</Text>  
+        <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. Peanuts'></TextInput>
+
+<Text> Emergency contact #1 name:</Text>  
+        <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'></TextInput>
+
+<Text> Emergency contact #1 phone number:</Text>  
+        <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 4071111111'></TextInput>
+
+<Text> Emergency contact #2 name:</Text>  
+        <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'></TextInput>
+
+<Text> Emergency contact #2 phone number:</Text>  
+        <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 4071111111'></TextInput>
+
+<Text> Primary doctor's name:</Text>  
+        <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe '></TextInput>
+
+<Text> Primary doctor's phone number</Text>  
+        <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 4071111111'></TextInput>
+
+
+        
+        
+        <Button
+          title="Turn On Bluetooth"
+          onPress={async () => {
+            const btState = await manager.state()
+            // test is bluetooth is supported
+            if (btState==="Unsupported") {
+              alert("Bluetooth is not supported");
+              return (false);
+            }
+            // enable if it is not powered on
+            if (btState!=="PoweredOn") {
+              await manager.enable();
+            } else {
+              await manager.disable();
+            }
+            return (true);
+          }}
+        />
+      </ScrollView>
+
+      <ScrollView style={{flex:2, padding:10}}>
+        <Text style={{fontWeight: "bold"}}>Scanned Devices ({deviceCount})</Text>
+        <FlatList
+          data={Object.values(scannedDevices)}
+          renderItem={({item}) => {
+            return (<Text>{`${item.name} (${item.id})`}</Text>)
+          }}
+        />
+        <Button
+          title="Scan Devices"
+          onPress={async () => {
+            const btState = await manager.state()
+            // test if bluetooth is powered on
+            if (btState!=="PoweredOn") {
+              alert("Bluetooth is not powered on");
+              return (false);
+            }
+            // explicitly ask for user's permission
+            const permission = await requestPermission();
+            if (permission) {
+              manager.startDeviceScan(null, null, async (error, device) => {
+                  // error handling
+                  if (error) {
+                    console.log(error);
+                    return
+                  }
+                  // found a bluetooth device
+                  if (device) {
+                    console.log(`${device.name} (${device.id})}`);
+                    const newScannedDevices = scannedDevices;
+                    newScannedDevices[device.id] = device;
+                    await setDeviceCount(Object.keys(newScannedDevices).length);
+                    await setScannedDevices(scannedDevices);
+                  }
+              });
+            }
+            return (true);
+          }}
+        />
+      </ScrollView>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+    
+  background: {
+      flex:1,
+      top: 200,
+      justifyContent: "center",
+      width: 350,
+      height: 1000,
+     
+  },
+
+  container: {
+      backgroundColor: "black",
+      flex: 1,
+  },
+  Group45text: {
+      width: 400,
+      height: 100,
+      bottom: 90,
+      fontSize: 30,
+      left: 170,
+      justifyContent: "center",
+      color: "white",
+  },
+  emergencyLogo: {
+      width: 350,
+      height: 450,
+      bottom: 30,
+      flexDirection: "row",
+      justifyContent: "center",
+  },
+  input: {
+      borderWidth: 1,
+      borderColor: '#777',
+      fontSize: 18,
+      padding: 8,
+      margin: 18,
+      width: 280,
+},
+input2: {
+  borderWidth: 1,
+  borderColor: 'black',
+  fontSize: 18,
+  padding: 8,
+  margin: 18,
+  width:350,
+},
+
+  
+});
+
+export default BluetoothScanner;
